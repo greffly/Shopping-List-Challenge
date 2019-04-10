c001582294460431710f89e1eb9ce7bdd518b2e4
Caitlyn Greffly <cgreffly@gmail.com>
2019-04-10 15:06:35 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function addItem(item) {
+  const newItem = `<li><span class="shopping-item">${item}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`;
+  $('.shopping-list').append(newItem);
+}
+
+$(function() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const item = $('#shopping-list-entry').val();
+    addItem(item);
+  });
+  $('.shopping-item-toggle').on('click', function(event) {
+    $(this).closest('li').toggleClass('shopping-item shopping-item__checked');
+  });
+  $('.shopping-item-delete').on('click', function(event) {
+    $(this).closest('.shopping-item').remove('li');
+  });  
+});
+
+
+$(document).ready()
